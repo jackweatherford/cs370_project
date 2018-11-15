aef158181af500a6616367554fa9b3208b0ea114
Kamuela96 <kamuela.1300@gmail.com>
2018-11-15 15:36:55 -0800

    deleted sound class

--- app/src/main/java/com/example/student/soundboard/MainActivity.java
+++ app/src/main/java/com/example/student/soundboard/MainActivity.java
@@ -6,23 +6,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton playButton;
 
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this, R.raw.test);
         playButton = findViewById(R.id.imageButton3);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(mp.isPlaying()){
+                    mp.pause();
+                    playButton.setImageResource(R.drawable.play);
+                                    }
+                else {
+                    mp.start();
+                    playButton.setImageResource(R.drawable.pause);
+                }
             }
         });
 
     }
     }
-}
+
+
 
