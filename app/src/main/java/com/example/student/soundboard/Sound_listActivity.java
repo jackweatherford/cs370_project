768e2a23f1ce8648f124d53e5bbdbf1bca75e8b7
kamuela96 <kamuela.1300@gmail.com>
2018-11-27 18:12:31 -0800

    switching activities now works

--- app/src/main/java/com/example/student/soundboard/Sound_listActivity.java
+++ app/src/main/java/com/example/student/soundboard/Sound_listActivity.java
@@ -1,14 +1,29 @@
 package com.example.student.soundboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Sound_listActivity extends AppCompatActivity {
+
+    private Button mainmenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soundlist);
+
+        mainmenuButton = findViewById(R.id.main_menu);
+        mainmenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MainActivity = new Intent(Sound_listActivity.this, MainActivity.class);
+
+                startActivity(MainActivity);
+            }
+        });
+
     }
 }
-
